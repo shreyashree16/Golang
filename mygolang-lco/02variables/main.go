2428defad1bcf30e39d08559ddfa72f8eed2a1f0
Shreya Shree <shreyashree@Shreyas-MacBook-Air.local>
2024-03-12 00:50:52 +0530

    first

---
+++ mygolang-lco/02variables/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var username string = "shreya"
+	fmt.Println(username)
+	fmt.Printf("Variable is of type: %T \n", username)
+
+	var Ischeck bool = true
+	fmt.Println(Ischeck)
+	fmt.Printf("Variable is of type: %T \n", Ischeck)
+}
